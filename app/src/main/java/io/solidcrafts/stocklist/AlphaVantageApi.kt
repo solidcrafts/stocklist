90031254c66389b3a8c30425994fe2294723fab9
Alex <alex@solidcrafts.io>
2022-04-14 02:55:50 +0300

    Initial commit

---
+++ app/src/main/java/io/solidcrafts/stocklist/AlphaVantageApi.kt
@@ -0,0 +1,10 @@
+package io.solidcrafts.stocklist
+
+class AlphaVantageApi {
+
+    companion object {
+        const val API_KEY = "DYARAVBUQXKOK0QJ"
+        const val BASE_URL = "https://www.alphavantage.co"
+    }
+
+}
